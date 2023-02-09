e0ee50c1a09f1a047c5f072bd9810ef61932588b
Kareem8691 <124925139+Kareem8691@users.noreply.github.com>
2023-02-09 23:44:23 +0200

    Add files via upload

---
+++ kk.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container')
+function cars(car){
+  img.src=(car)
+}
+function zeft(color){
+  container.style.background = color;
+}
